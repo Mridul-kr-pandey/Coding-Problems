727f9c1e7b85fd3a3c71d11e6f379b676925583e
Mridul-kr-pandey <154259945+Mridul-kr-pandey@users.noreply.github.com>
2026-01-24 13:05:50 +0530

    Time: 1 ms (95.95%), Space: 43 MB (65.63%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -6,13 +6,12 @@ class Solution {
         for (int i=0;i<chars.length-2;i++){
             char ch0=chars[i];
             char ch1=chars[i+1];
-            char ch2 = chars[i + 2];
+            char ch2=chars[i+2];
 
-            if (ch0 != ch1 && ch0 != ch2 && ch1 != ch2) {
+            if (ch0!=ch1 && ch0!=ch2 && ch1!=ch2){
                 count++;
             }
         }
-
         return count;
     }
 }
