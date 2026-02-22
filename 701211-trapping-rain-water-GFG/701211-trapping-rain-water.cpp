7862484145a4b34b6f471628c039ca697437e4a1
Mridul-kr-pandey <154259945+Mridul-kr-pandey@users.noreply.github.com>
2026-02-23 00:37:56 +0530

    Added solution - LeetHub

---
+++ 701211-trapping-rain-water-GFG/701211-trapping-rain-water.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int maxWater(vector<int> &height) {
+        // code here
+        int left = 0;
+        int right = height.size() - 1;
+        int leftMax = height[left];
+        int rightMax = height[right];
+        int water = 0;
+
+        while (left < right) {
+            if (leftMax < rightMax) {
+                left++;
+                leftMax = max(leftMax, height[left]);
+                water += leftMax - height[left];
+            } else {
+                right--;
+                rightMax = max(rightMax, height[right]);
+                water += rightMax - height[right];
+            }
+        }
+
+        return water; 
+    }
+};
