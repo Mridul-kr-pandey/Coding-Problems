430df2accc83c0517251616c8c3f8d074a311740
Mridul-kr-pandey <154259945+Mridul-kr-pandey@users.noreply.github.com>
2026-04-18 11:55:24 +0530

    Time: 1 ms (7.18%), Space: 8.1 MB (50.45%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int n=word.size();
+        if(n==1) return true;
+        
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(isupper(word[i])){
+                count++;
+            }
+        }
+        if(count==n || count==0) return true;
+        if(count==1 && isupper(word[0])) return true;
+        return false;
+    }
+};
