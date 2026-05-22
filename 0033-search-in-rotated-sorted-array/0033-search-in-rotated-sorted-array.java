4a6ee2ebdacee60817c0fd7aebf3773d1b8cfbcd
Mridul-kr-pandey <154259945+Mridul-kr-pandey@users.noreply.github.com>
2026-05-23 01:51:44 +0530

    Time: 0 ms (100%), Space: 43.8 MB (45.14%) - LeetHub - Time: O(log n), Space: O(1)

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -6,18 +6,18 @@ class Solution {
 
             if(nums[mid]==target) return mid;
 
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<nums[mid]){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
                 }
             }
-            else {
-                if (nums[mid] < target && target <= nums[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
+            else{
+                if(nums[mid]<target && target<=nums[high]){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
                 }
             }
         }
